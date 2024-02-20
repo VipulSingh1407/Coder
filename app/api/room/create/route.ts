fc5ae6bd3bce23d45342b0f13b70547226f027b6
VipulSingh1407 <vipulsingh9369384252@gmail.com>
2024-02-20 23:52:36 +0530

    Update route.ts

--- app/api/room/create/route.ts
+++ app/api/room/create/route.ts
@@ -3,28 +3,36 @@ import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { nanoid } from 'nanoid'
 import { authOptions } from '../../auth/[...nextauth]/route'
- 
-export async function GET() {
-  const session = await getServerSession(authOptions)
-  if(!session) return NextResponse.json({message: "Unauthenticated"}, {status: 401})
-  const userId = session?.user?.id
-  const isPremium = session?.user?.isPremium
-  const roomId = nanoid(12)
 
-  try{
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions)
+    if (!session) {
+      return NextResponse.json({ message: "Unauthenticated" }, { status: 401 })
+    }
+    const userId = session?.user?.id
+    const isPremium = session?.user?.isPremium
+    const roomId = nanoid(12)
+
     const obj = {
       creator: userId,
       joinable: true,
       limit: isPremium ? 16 : 6
     }
     const str = JSON.stringify(obj)
-    await kv.set(roomId, str)
-    const room = await kv.get(roomId)
+
+    // Parallelize KV set and KV get operations
+    const setPromise = kv.set(roomId, str)
+    const getPromise = kv.get(roomId)
+
+    await Promise.all([setPromise, getPromise])
+
+    const room = await getPromise
+
     console.log(room)
-    return NextResponse.json({ message: "Created Successfully", roomId})
-  }
-  catch(e){
+    return NextResponse.json({ message: "Created Successfully", roomId })
+  } catch (e) {
     console.log(e, "Error in room creation")
-    return NextResponse.json({ mesage: "Could Not Create Room" }, {status: 500})
+    return NextResponse.json({ message: "Could Not Create Room" }, { status: 500 })
   }
 }
